ea35fd20c9c67c347d3048e3cf472b226a0da66f
strikeouts27 <99090208+strikeouts27@users.noreply.github.com>
2026-05-28 18:53:08 -0500

    debugging attempt

---
+++ ScrumMovieTheater/Models/Schedule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ScrumTheater
+{
+	public class ScrumTheater
+	{
+		public required string TheaterName { get; set; }
+        public DateTime ShowtimeUtc { get; set; }
+    }
+}
+
