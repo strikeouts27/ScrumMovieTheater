88a228fe798a3e884671a766fa41310df2436849
strikeouts27 <99090208+strikeouts27@users.noreply.github.com>
2026-05-28 23:42:30 -0500

    movie view page

--- ScrumMovieTheater/Controllers/MoviesController.cs
+++ ScrumMovieTheater/Controllers/MoviesController.cs
@@ -1,19 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
-using ScrumMovieTheater.Services;
-
+    
 namespace ScrumMovieTheater.Controllers;
 
 public class MoviesController : Controller
-{
-    private readonly IMovieCatalog movieCatalog;
-
-    public MoviesController(IMovieCatalog movieCatalog)
-    {
-        this.movieCatalog = movieCatalog;
-    }
-
+{   
     public IActionResult Index()
     {
-        return View(movieCatalog.GetAll());
+        return View();
     }
 }
