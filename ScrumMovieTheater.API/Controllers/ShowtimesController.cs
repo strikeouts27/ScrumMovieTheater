a5822de027885119a953675b97a180c96d4e94a0
strikeouts27 <99090208+strikeouts27@users.noreply.github.com>
2026-06-06 14:20:45 -0500

    ShowtimesControllers blank file created to showcase a need for HTTP METHODS

---
+++ ScrumMovieTheater.API/Controllers/ShowtimesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScrumMovieTheater.API.Controllers
+{
+    public class ShowtimesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
