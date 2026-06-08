6cc3376934a5557b44c0d1b9ecbcd824aa57bd1d
strikeouts27 <99090208+strikeouts27@users.noreply.github.com>
2026-06-08 14:30:18 -0500

    Revert "Created a scrum movie theater mysql script."
    This reverts commit 57dd9cb87569b6aa75004833f7aa095aa8ca8a3b.

--- ScrumMovieTheater.API/providers/ScrumMovieTheaterContext.cs
+++ ScrumMovieTheater.API/providers/ScrumMovieTheaterContext.cs
@@ -26,8 +26,6 @@ public partial class ScrumMovieTheaterContext : DbContext
 
     public virtual DbSet<Showtime> Showtimes { get; set; }
 
-    // TO DO THEATER
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
         if (!optionsBuilder.IsConfigured) {
             
--- ScrumMovieTheater/Models/TheaterViewModel.cs
+++ ScrumMovieTheater/Models/TheaterViewModel.cs
@@ -3,10 +3,7 @@ namespace Theater {
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-        public required string Address { get; set; }
-        public required string Description { get; set; }
-
-         
-        
+        public required string Location { get; set; }
+        public int Capacity { get; set; }
     }
 }
