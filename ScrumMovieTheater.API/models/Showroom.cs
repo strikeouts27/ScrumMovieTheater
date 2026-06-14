2cfe24678bc3dc8d3cfdc4dce48d0bb1526055dd
strikeouts27 <99090208+strikeouts27@users.noreply.github.com>
2026-06-14 15:56:33 -0500

    use the scaffold command to update the database context to the most up to date version before operation.

--- ScrumMovieTheater.API/models/Showroom.cs
+++ ScrumMovieTheater.API/models/Showroom.cs
@@ -1,11 +1,23 @@
-﻿namespace ScrumMovieTheater.API.models
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScrumMovieTheater.API.Models;
+
+public partial class Showroom
 {
-    public class Showroom
-    {
-        public int idShowroom { get; set; }
-        public int Capacity { get; set; }
-        public Movie Movie { get; set; }
-        public string ShowroomNumber { get; set; }
-        public Theater Theater { get; set; }
-    }
+    public int IdShowroom { get; set; }
+
+    public int Capacity { get; set; }
+
+    public int Movie { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Theater { get; set; }
+
+    public virtual Movie MovieNavigation { get; set; } = null!;
+
+    public virtual ICollection<Showtime> Showtimes { get; set; } = new List<Showtime>();
+
+    public virtual Theater TheaterNavigation { get; set; } = null!;
 }
--- ScrumMovieTheater.API/models/Theater.cs
+++ ScrumMovieTheater.API/models/Theater.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace ScrumMovieTheater.API.models;
+namespace ScrumMovieTheater.API.Models;
 
-public class Theater
+public partial class Theater
 {
-    public int idTheater { get; set; }
-    public string Name { get; set; }
+    public int IdTheater { get; set; }
 
-    public string Address { get; set; }
+    public string? Name { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual ICollection<Showroom> Showrooms { get; set; } = new List<Showroom>();
 }
-
-
-
