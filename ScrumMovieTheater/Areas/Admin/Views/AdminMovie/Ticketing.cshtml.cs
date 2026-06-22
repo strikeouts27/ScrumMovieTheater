e006a1331880711b0aed1f1935a453c3933e267c
eugenepro8 <99391556+eugenepro8@users.noreply.github.com>
2026-06-22 13:24:15 -0500

    i created a ticket page

---
+++ ScrumMovieTheater/Areas/Admin/Views/AdminMovie/Ticketing.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScrumMovieTheater.Areas.Admin.Views.AdminMovie.Pages;
+
+public class TicketingModel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
